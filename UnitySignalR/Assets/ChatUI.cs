39ff4ff0b57ec8abff72a607eb70a9c2f96ab2e3
hktrollgames <hktrollgames@gmail.com>
2021-09-16 21:39:27 +0900

    채팅 UI작업중

---
+++ UnitySignalR/Assets/ChatUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatUI : MonoBehaviour
+{
+    public Text textBaseItem;
+    public Text currentChannel;
+    public InputField chatInputFiled;
+    public InputField newChannelInputFiled;
+    public Button sendButton;
+    public Button changeChannelButton;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
